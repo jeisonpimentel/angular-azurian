30514d2224dc1c0fa91c1ff8099f9e55e8fff176
Alexisveraguas <al.veraguas@duocuc.cl>
2022-06-26 19:29:02 -0400

    formulario crear adopcion

--- src/app/adopcion/create-adopcion/create-adopcion.component.ts
+++ src/app/adopcion/create-adopcion/create-adopcion.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-create-adopcion',
-  templateUrl: './create-adopcion.component.html',
-  styleUrls: ['./create-adopcion.component.css']
+  templateUrl: './create-adopcion.component.html'
 })
 export class CreateAdopcionComponent implements OnInit {
 
@@ -12,4 +12,8 @@ export class CreateAdopcionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  crearAdopcion() : void {
+    Swal.fire('Hemos recibido su aviso, en breve se publicará en la sección "Adopciones"');
+        
+  }
 }
