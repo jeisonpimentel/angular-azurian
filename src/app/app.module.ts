f306ecd96bd44dc044d1b44b16d6261414400e45
jvierrr <ja.pailahueque@duocuc.cl>
2022-06-26 20:55:00 -0400

    formulario extravio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { RouterModule } from '@angular/router';
+import { ExtravioComponent } from './extravio/extravio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ExtravioComponent,
   ],
   imports: [
     BrowserModule,
