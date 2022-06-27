828aeef6ea6fe6a11e40f8344db1a0e7bd9b3b97
Kwangho1712 <84871898+Kwangho1712@users.noreply.github.com>
2022-06-26 23:10:12 -0400

    'S1

--- src/app/veterinario/form-veterinario/form-veterinario.component.ts
+++ src/app/veterinario/form-veterinario/form-veterinario.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-form-veterinario',
   templateUrl: './form-veterinario.component.html',
 })
-export class FormVeterinarioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FormVeterinarioComponent implements OnInit{ 
+  
+  ngOnInit() {
+  
   }
 
+  crearVeterinario() : void {
+    Swal.fire('Veterinario Creado');
+
+  }
 }
--- src/app/veterinario/veterinario.module.ts
+++ src/app/veterinario/veterinario.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 import { MaterialModule } from '../material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -17,6 +17,7 @@ import { VeterinarioRoutingModule } from './veterinario-routing.module';
     FlexLayoutModule,
     RouterModule,
     VeterinarioRoutingModule,
+    ReactiveFormsModule
   ], exports:[
     RouterModule
   ]
